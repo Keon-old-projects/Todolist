2f89e107b43946851192afa335161b66d96090fa
Keon <h21816595@gmail.com>
2022-11-05 13:36:35 +0800

    Final Code

--- scripts/js.js
+++ scripts/js.js
@@ -47,12 +47,12 @@ add.addEventListener("click", e => {
             let todoItem = e.target.parentElement;
             todoItem.style.animation = "scaleDown 0.2s forwards";
             todoItem.addEventListener("animationend", () => {
-                let text=todoItem.children[0].innerText;
-                let myListArray=JSON.parse(localStorage.getItem('list'));
-                myListArray.forEach((item,index)=>{
-                    if(item.todoText ==text){
-                        myListArray.splice(index,1);
-                        localStorage.setItem('list',JSON.stringify(myListArray));
+                let text = todoItem.children[0].innerText;
+                let myListArray = JSON.parse(localStorage.getItem('list'));
+                myListArray.forEach((item, index) => {
+                    if (item.todoText == text) {
+                        myListArray.splice(index, 1);
+                        localStorage.setItem('list', JSON.stringify(myListArray));
                     }
                 })
                 todoItem.remove();
@@ -87,58 +87,120 @@ add.addEventListener("click", e => {
     form.children[0].value = "";
     section.appendChild(todo);
 })
+loadData();
+function loadData() {
 
+    let myList = localStorage.getItem('list');
+    if (myList != null) {
+        let myListArray = JSON.parse(myList);
+        myListArray.forEach(item => {
+            let todo = document.createElement("div");
+            todo.classList.add("todo");
+            let text = document.createElement("p");
+            text.classList.add('todo-text');
+            text.innerText = item.todoText;
+            let time = document.createElement('p');
+            time.classList.add('todo-time');
+            time.innerText = item.todoMon + ' / ' + item.todoDate;
+            todo.appendChild(text);
+            todo.appendChild(time);
 
-let myList = localStorage.getItem('list');
-if (myList != null) {
-    let myListArray = JSON.parse(myList);
-    myListArray.forEach(item => {
-        let todo = document.createElement("div");
-        todo.classList.add("todo");
-        let text = document.createElement("p");
-        text.classList.add('todo-text');
-        text.innerText = item.todoText;
-        let time = document.createElement('p');
-        time.classList.add('todo-time');
-        time.innerText = item.todoMon + ' / ' + item.todoDate;
-        todo.appendChild(text);
-        todo.appendChild(time);
+            //勾選
+            let checkButton = document.createElement("button");
+            checkButton.classList.add('check');
+            checkButton.innerHTML = '<i class="fa-solid fa-check"></i>'
 
-        //勾選
-        let checkButton = document.createElement("button");
-        checkButton.classList.add('check');
-        checkButton.innerHTML = '<i class="fa-solid fa-check"></i>'
-        
-        checkButton.addEventListener("click", e => {
-            let todoItem = e.target.parentElement;
-            todoItem.classList.toggle("done");
-        })
-        // 垃圾
-        let trashButton = document.createElement('button');
-        trashButton.classList.add("trash");
-        trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-        
-        trashButton.addEventListener('click', e => {
-            let todoItem = e.target.parentElement;
-            todoItem.style.animation = "scaleDown 0.2s forwards";
-            todoItem.addEventListener("animationend", () => {
-                let text=todoItem.children[0].innerText;
-                let myListArray=JSON.parse(localStorage.getItem('list'));
-                myListArray.forEach((item,index)=>{
-                    if(item.todoText ==text){
-                        myListArray.splice(index,1);
-                        localStorage.setItem('list',JSON.stringify(myListArray));
-                    }
-                })
-                todoItem.remove();
+            checkButton.addEventListener("click", e => {
+                let todoItem = e.target.parentElement;
+                todoItem.classList.toggle("done");
             })
-        })
+            // 垃圾
+            let trashButton = document.createElement('button');
+            trashButton.classList.add("trash");
+            trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
 
-        // 新增icons
-        todo.appendChild(checkButton);
-        todo.appendChild(trashButton);
-        // 新增清單
-        
-        section.appendChild(todo);
-    });
+            trashButton.addEventListener('click', e => {
+                let todoItem = e.target.parentElement;
+                todoItem.style.animation = "scaleDown 0.2s forwards";
+                todoItem.addEventListener("animationend", () => {
+                    let text = todoItem.children[0].innerText;
+                    let myListArray = JSON.parse(localStorage.getItem('list'));
+                    myListArray.forEach((item, index) => {
+                        if (item.todoText == text) {
+                            myListArray.splice(index, 1);
+                            localStorage.setItem('list', JSON.stringify(myListArray));
+                        }
+                    })
+                    todoItem.remove();
+                })
+            })
+
+            // 新增icons
+            todo.appendChild(checkButton);
+            todo.appendChild(trashButton);
+            // 新增清單
+
+            section.appendChild(todo);
+        });
+    }
 }
+
+function mergeTime(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (Number(arr1[i].todoMon) > Number(arr2[j].todoMon)) {
+            result.push(arr2[j]);
+            j++;
+        } else if (Number(arr1[i].todoMon) < Number(arr2[j].todoMon)) {
+            result.push(arr1[i]);
+            i++;
+        } else if (Number(arr1[i].todoMon) == Number(arr2[j].todoMon)) {
+            if (Number(arr1[i].todoDate) > Number(arr2[j].todoDate)) {
+                result.push(arr2[j]);
+                j++;
+            } else {
+                result.push(arr1[i]);
+                i++;
+            }
+        }
+    }
+
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+    return result;
+}
+
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    } else {
+        let middle = Math.floor(arr.length / 2);
+        let rigth = arr.slice(0, middle);
+        let left = arr.slice(middle, arr.length);
+        return mergeTime(mergeSort(rigth), mergeSort(left));
+    }
+}
+
+
+let sortButton = document.querySelector(".btn");
+sortButton.addEventListener('click', () => {
+    //sort data
+    let sortedArray = mergeSort(JSON.parse(localStorage.getItem("list")));
+    localStorage.setItem('list', JSON.stringify(sortedArray));
+
+    //remove data
+    let len = section.children.length;
+    for (let i = 0; i < len; i++) {
+        section.children[0].remove();
+    }
+    loadData();
+})
